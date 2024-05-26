c146324d81bcfab88508f298a506587c51d7eb7b
Ali Emre Şafak <aliemresafak@gmail.com>
2024-05-26 17:20:37 +0300

    feat: add settings

---
+++ api/settings/views.py
@@ -0,0 +1,22 @@
+from rest_framework import status, views
+from rest_framework.response import Response
+
+from api.settings.serializers import SettingsSerializer
+from settings.models import DefaultCurrencyChoices, Settings
+
+
+class SettingsAPIView(views.APIView):
+    serializer_class = SettingsSerializer
+
+    def get(self, request):
+        settings = Settings.objects.first()
+        serializer = SettingsSerializer(settings)
+        return Response(serializer.data)
+
+    def patch(self, request):
+        settings = Settings.objects.first()
+        serializer = SettingsSerializer(settings, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- api/urls.py
+++ api/urls.py
@@ -2,8 +2,12 @@ from django.urls import include, path
 from rest_framework import routers
 
 from api.customer.views import CustomerViewSet
+from api.settings.views import SettingsAPIView
 
 api_router = routers.DefaultRouter()
 api_router.register("customers", CustomerViewSet)
 
-urlpatterns = [path("", include(api_router.urls))]
+urlpatterns = [
+    path("", include(api_router.urls)),
+    path("settings/", SettingsAPIView.as_view(), name="settings"),
+]
