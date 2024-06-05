e716a8c5e9fbcb4c296b54a7aba203d87569390e
Ali Emre Şafak <aliemresafak@gmail.com>
2024-06-06 00:12:19 +0300

    feat: add signal for repair status

--- repair/signals.py
+++ repair/signals.py
@@ -2,12 +2,30 @@ from django.db.models import F
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from repair.models import Repair
+from repair.models import Repair, RepairStatus, RepairStatusChoices
 from spare_part.models import SparePart
 
 
 @receiver(post_save, sender=Repair)
 def create_repair(sender, instance, created, **kwargs):
-    if created:
+    if not created:
+        print("updated")
+        if instance.statuses.last().status == "REPAIRED":
+            breakpoint()
+            spare_part = SparePart.objects.filter(uuid=instance.spare_part.uuid)
+            spare_part.update(temp_amount=F("temp_amount") - 1, left_amount=F("left_amount") - 1)
+    else:
+        print("created")
+        repair_status = RepairStatus.objects.create(
+            status=RepairStatusChoices.WAITING_REPAIR, title="Repair created", repair=instance
+        )
         spare_part = SparePart.objects.filter(uuid=instance.spare_part.uuid)
-        spare_part.update(left_amount=F("left_amount") - 1)
+        spare_part.update(temp_amount=F("temp_amount") + 1)
+
+
+@receiver(post_save, sender=RepairStatus)
+def update_repair_status(sender, instance, created, **kwargs):
+    if created:
+        if instance.status == "REPAIRED":
+            spare_part = SparePart.objects.filter(uuid=instance.repair.spare_part.uuid)
+            spare_part.update(temp_amount=F("temp_amount") - 1, left_amount=F("left_amount") - 1)
