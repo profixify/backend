c6a38bc40cc0ea18e456e591f09a772514c99acc
Ali Emre Şafak <aliemresafak@gmail.com>
2024-06-06 14:10:35 +0300

    feat: add signal for repair status

--- repair/signals.py
+++ repair/signals.py
@@ -8,17 +8,8 @@ from spare_part.models import SparePart
 
 @receiver(post_save, sender=Repair)
 def create_repair(sender, instance, created, **kwargs):
-    if not created:
-        print("updated")
-        if instance.statuses.last().status == "REPAIRED":
-            breakpoint()
-            spare_part = SparePart.objects.filter(uuid=instance.spare_part.uuid)
-            spare_part.update(temp_amount=F("temp_amount") - 1, left_amount=F("left_amount") - 1)
-    else:
-        print("created")
-        repair_status = RepairStatus.objects.create(
-            status=RepairStatusChoices.WAITING_REPAIR, title="Repair created", repair=instance
-        )
+    if created:
+        RepairStatus.objects.create(status=RepairStatusChoices.WAITING_REPAIR, title="Repair created", repair=instance)
         spare_part = SparePart.objects.filter(uuid=instance.spare_part.uuid)
         spare_part.update(temp_amount=F("temp_amount") + 1)
 
